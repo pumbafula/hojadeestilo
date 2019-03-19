95dba2fa367ab6b5bf6a09f557e901529fcf611c
cetecom <cetecom@LCV11014>
2019-03-19 14:41:15 -0300

    hehehe

---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="list-group">
+<a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Home</a>
+<a class="list-group-item" id="menusolicitudes" href="#"><i class="fa fa-file-o fa-2x"></i> Solicitudes</a>
+<a class="list-group-item submenu" id="menuvacaciones" href="<?=URLBASE;?>contenido/vacaciones.php"><i class="fa fa-sun-o fa-2x"></i> Vacaciones</a>
+<a class="list-group-item" href="#"><i class="fa fa-birthday-cake fa-2x"></i> Cumpleaños</a>
+<a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> Certificados</a>
+<a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> Resumen</a>
+</div>
+
+
+
+
